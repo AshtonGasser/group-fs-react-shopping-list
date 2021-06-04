aefa01752fcabfbeeaa05e3d2da928a4e410198d
Ashton Gasser <ashtongasser@gmail.com>
2021-06-04 11:04:39 -0500

    spelling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import axios from "axios";
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+    // post
+    const addItem = (newItem) => {
+        console.log(newItem);
+         // ⬇ POST your data here
+         axios.post("/list", newItem)
+         .then(response => {
+             console.log('response', response);
+             // get function here ***
+         }).catch(error => {
+             console.log(error)
+         })
+    }
     return (
         <div className="App">
             <Header />
