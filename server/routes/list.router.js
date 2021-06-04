34933b4e7636f8d5d4ef28dee71a1e10df5f570c
Jacob Motes <jmotes2583@gmail.com>
2021-06-04 10:50:30 -0500

    GET requests added on client and server

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -7,7 +7,24 @@ const pool = require('../modules/pool.js');
 
 
 // GET⬇
+router.get('/', (req, res) => {
 
+    // queryText grabs our table data and organizes it aphabetically by the "name" vals
+    let queryText = `SELECT * FROM shopping_carts ORDER BY "name" ASC;`;
+    // need to tell the database what data we want to retrieve
+    pool.query(queryText)
+    .then( result => {
+        // result is what we will get from the database
+        console.log('The result from the get route is ', result);
+        // send to client
+        res.send(result.rows);
+
+        // catch errors
+    }).catch ( err => {
+        console.log('There was an error in the GET route', err);
+        res.sendStatus(500);
+    })
+})
 
 // POST⬇
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,32 @@ import './App.css';
 
 
 function App() {
+    
+    // create a function that will GET the data from the server
+    const getItems = () => {
+    // use axios to contact the server and specify what route you want to receive data from 
+        axios.get('/list')
+        .then(response => 
+            // responds with DB data
+            console.log('Response from server', response)
+
+            // set the data here
+        )
+        // catch for error
+        .catch(error => {
+            console.log('Error getting data from server', error);
+        })
+    }
+
+
+
+
+
+
+
+
+
+
     return (
         <div className="App">
             <Header />
