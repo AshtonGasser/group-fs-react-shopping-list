625b5342512c03014c5fb4b47b76a9c10271e007
Ashton Gasser <ashtongasser@gmail.com>
2021-06-04 10:38:27 -0500

    post feature front/back

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -10,7 +10,19 @@ const pool = require('../modules/pool.js');
 
 
 // POST⬇
+router.post('/', (req, res) => {
+    const newItem = req.body
+    const sqlText = `INSERT INTO shopping_cart (name, quantity, unit) VALUES ($1, $2, $3);`;
 
+    pool.query(sqlText, [newItem.name, newItem.quantity, newItem.unit])
+        .then ((result ) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${error}`);
+        res.sendStatus(500);
+    });
+})
 
 
 
