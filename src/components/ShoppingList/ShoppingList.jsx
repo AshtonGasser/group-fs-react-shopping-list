0e822fc5d102b138c4eaf16dfb06db4f7842c486
pthomas0309 <preston.thomas355@gmail.com>
2021-06-04 14:27:05 -0500

    clear list on click of clear button

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,13 +1,20 @@
 import ShoppingItem from "../ShoppingItem/ShoppingItem"
 // import {Button} from 'react-bootstrap/Button';
 function ShoppingList ({list, deleteItem}){
+
+    const clearPage = () => {
+
+        for (let listItem of list){
+            deleteItem(listItem.id);
+        }
+    }
     
     
     return (
         <>
             <h3>Shopping List</h3>
             <button type = "button" className = "btn btn-neutral" id = "resetBtn">Reset</button>
-            <button type = "button" className = "btn btn-danger" id = "clearBtn">Clear</button>
+            <button type = "button" className = "btn btn-danger" id = "clearBtn" onClick={clearPage}>Clear</button>
             <div>
                 {list.map(list => (
                     <ShoppingItem key={list.id} list={list} deleteItem={deleteItem} />
